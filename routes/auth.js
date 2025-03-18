0432fc6b2b5eac566ec4194f288e0baad260b816
trungdt226 <trungdtbn.work@gmail.com>
2025-03-18 20:56:56 +0700

    Update auth.js
    them sign in vs update ttin user

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const pool = require('../config/db');
+const authenticateToken = require('../middleware/auth');
 
 // Login Endpoint
 router.post('/login', async (req, res) => {
@@ -60,5 +61,97 @@ router.post('/logout', (req, res) => {
   res.json({ message: 'Logout successful' });
 });
 
+//Sign-in Endpoint
+router.post('/register', async (req, res)=> {
+  try{
+    const {username, password, email} = req.body;
+
+    //Validate Sign-in Credentials
+    if (!username || !password || !email){
+      return res.status(400).json({ error: 'Invalid Credentials'});
+   } 
+   //check if username exists
+   const [existingUser] = await pool.query(
+    'SELECT user_id FROM users WHERE username = ?',
+    [username]
+   );
+   if (existingUser.length>0){
+    return res.status(400).json({ error: 'Username already exists'});
+   }
+   
+   //Hash password with bycrypt
+   const satlRounds = 10;
+   const hashedPassword = await bcrypt.hash(password, satlRounds);
+   
+   //add user to db
+   await pool.query(
+    'INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)',
+    [username,email,hashedPassword]
+   );
+  
+   res.json({ message: 'User registered succesfully'});
+  } catch(error){
+    console.error(error);
+    res.status(500).json({ error: 'Server error'})
+  }
+});
+
+//update user info
+router.patch('/edit', authenticateToken, async (req, res)=>{
+  try{
+    const userId = req.user.user_id;
+    const {
+      email,
+      full_name,
+      address,
+      phone_number,
+      password
+    } = req.body;
+  
+    const fields = [];
+    const values = [];
+
+    if(email){
+      fields.push('email=?');
+      values.push(email);
+    }
+
+    if(full_name){
+      fields.push('full_name=?');
+      values.push(full_name)
+    }
+
+    if(address){
+      fields.push('address=?');
+      values.push(address)
+    }
+
+    if(phone_number){
+      fields.push('phone_number=?');
+      values.push(phone_number)
+    }
+    //hash the password if the user wants to change it
+    if(password){
+      const satlRounds = 10;
+      const hashedPassword = await bcrypt.hash(password,satlRounds);
+      fields.push('password_hash=?');
+      values.push(hashedPassword)
+    }
+
+    if(fields.length === 0){
+      return res.status(400).json({ error: 'No information to be update'});
+    }
+
+    //update the db
+    const sql = `UPDATE users SET ${fields.join(', ')} WHERE user_id = ?`;
+    values.push(userId);
+    await pool.query(sql, values);
+
+    res.json({ message: 'Information updated successfully'});
+  } catch(error){
+    console.error(error);
+    res.status(500).json({ error:'Server error'});
+  }
+});
 
 module.exports = router;
